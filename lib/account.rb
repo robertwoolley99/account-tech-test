5b4ce70aca0168c74bb4ec0f1b6219e44673a91f
Robert Woolley <rob@robertwoolley.co.uk>
2019-12-16 16:09:37 +0000

    Refactored for currency formatting

--- lib/account.rb
+++ lib/account.rb
@@ -6,28 +6,28 @@ class Account
   end
 
   def deposit(value, date)
-    if value % 1 != 0
-      deposit_as_a_string = value.to_s
-    else
-      deposit_as_a_string = value.to_s + ".00"
-    end
     @balance += value
-
-    if @balance % 1 != 0
-      balance_as_a_string = @balance.to_s
-    else
-      balance_as_a_string = @balance.to_s + ".00"
-    end
+    deposit_as_a_string = format_currency(value)
+    balance_as_a_string = format_currency(@balance)
 
     this_deposit = date + " || "+ deposit_as_a_string + " || || " + balance_as_a_string
     @transactions.push(this_deposit)
   end
 
-def statement
-  output = ""
-  @transactions.each do |line_item|
-    output += line_item
+  def statement
+    output = ""
+    @transactions.each do |line_item|
+      output += line_item
+    end
   end
-end
+
+  private
+
+  def format_currency(input)
+    output = input.to_s
+    if input % 1 == 0
+      output += ".00"
+    end
+  end
 
 end
