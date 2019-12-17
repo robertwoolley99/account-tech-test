729ed8552912184d76b7401839dcd0f59a44817c
Robert Woolley <rob@robertwoolley.co.uk>
2019-12-17 16:09:45 +0000

    Added functionality and tests for currency formatter and line formatter in transactions

--- lib/transactions.rb
+++ lib/transactions.rb
@@ -1,8 +1,27 @@
 # frozen_string_literal: true
 
+# Object to store and process transactions.
 class Transactions
-
   def date_reformat(date_entered)
     date_entered.gsub(/-/, '/')
+  end
+
+
+  def format_currency(input)
+    output = input.to_s
+    return output unless (input % 1).zero?
+
+    output + '.00'
+  end
+
+  def line_formatter(transaction_type, date_reformatted, \
+                     value_as_a_string, balance_as_a_string)
+    middle_bit_of_transaction = if transaction_type == 'deposit'
+                                  value_as_a_string + ' || || '
+                                else
+                                  '|| ' + value_as_a_string + ' || '
+                                end
+    date_reformatted + ' || ' + middle_bit_of_transaction + \
+      balance_as_a_string
   end
 end
--- spec/transactions_spec.rb
+++ spec/transactions_spec.rb
@@ -7,7 +7,32 @@ describe Transactions do
   context 'Check that items passed are correctly converted' do
     it 'converts date with dashes to correct format' do
       date_reformat = trans.date_reformat('01-01-2020')
-      expect(date_reformat).to include('01/01/2020')
+      expect(date_reformat).to eq('01/01/2020')
+    end
+    it 'reformats currency to insert decimal zeros' do
+      currency_reformatted = trans.format_currency(100)
+      expect(currency_reformatted).to eq('100.00')
+    end
+    it 'converts currency with decimals to text without changing it' do
+      currency_reformatted = trans.format_currency(234.56)
+      expect(currency_reformatted).to eq('234.56')
     end
   end
+end
+# Split object block - Rubocop complaining.
+describe Transactions do
+  let(:trans) { Transactions.new }
+  context 'Check formatting for records'
+  it 'writes a deposit correctly' do
+    test_deposit = trans.line_formatter('deposit', '31/12/2019', '1000.00', \
+                                        '1000.00')
+    expect(test_deposit).to eq('31/12/2019 || 1000.00 || || 1000.00')
+  end
+  it 'writes a withdrawal correctly' do
+    test_withdrawal = trans.line_formatter('withdrawal', '08/09/1987', \
+                                           '500.00', '756.06')
+    expect(test_withdrawal).to eq('08/09/1987 || || 500.00 || 756.06')
+  end
+
+
 end
